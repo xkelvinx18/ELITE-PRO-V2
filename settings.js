877ec427ba1709528e8b68e2b3a60c982d9d597e
xkelvinx18 <kelfut25@gmail.com>
2025-05-21 13:41:39 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -8,8 +8,8 @@ global.location = "Port Harcourt, Nigeria" //ur location
 
 //new settings 
 global.botname = 'ELITE-PRO-V2' //ur bot name
-global.ownernumber = '2348109263390' //ur owner number
-global.ownername = 'ELITE-PRO-V2' //ur owner name
+global.ownernumber = '233202683923' //ur owner number
+global.ownername = 'jaseh' //ur owner name
 global.websitex = "https://youtube.com/@elitepro-empire"
 global.elitepropp = 'https://i.ibb.co/jk0ynvbn/7a8c4b5b617fa11a1e9a61190f427546.jpg' // Change this image link to yours.
 
